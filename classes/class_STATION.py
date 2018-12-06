64b5e25fba39a740a224eb824ee35811dab4d4a1
DoorDool <dorindavandendool@hotmail.nl>
2018-12-06 23:39:37 +0100

    grafiek1a/brandom2x

--- classes/class_STATION.py
+++
@@ -1,7 +0,0 @@
-class STATION:
-    def __init__(self, name, critic, connections, x, y):
-        self.name = name
-        self.critic = critic 
-        self.x = x
-        self.y = y
-        self.connections = connections
